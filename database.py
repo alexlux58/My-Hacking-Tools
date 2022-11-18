9f1a291aa007ac61edbd9db0919803ecc4a3c960
Alex Lux <alexlux@live.com>
2022-11-17 21:27:10 -0800

    Created a query button to show all records on the Database

--- database.py
+++ database.py
@@ -8,18 +8,6 @@ root.iconbitmap('python_icon.png')
 root.geometry("600x600")
 
 
-conn = sqlite3.connect('address_book.db')
-
-cursr = conn.cursor()
-cursr.execute("""CREATE TABLE addresses (
-    first_name text,
-    last_name text,
-    address text,
-    city text,
-    zipcode integer
-    )""")
-
-
 f_name = Entry(root, width=30)
 f_name.grid(row=0, column=1, padx=20)
 
@@ -56,19 +44,81 @@ state_label.grid(row=4, column=0)
 zipcode_label = Label(root, text="ZIP")
 zipcode_label.grid(row=5, column=0)
 
+
+
+
+
+
 def submit():
+    conn = sqlite3.connect('address_book.db')
+    cursr = conn.cursor()
+    
+    '''
+    cursr.execute("""CREATE TABLE addresses (
+    first_name text,
+    last_name text,
+    address text,
+    city text,
+    state text,
+    zipcode integer
+    )""")
+    '''
+    
+    cursr.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :address, :city, :state, :zipcode)",
+                  {
+                      'f_name': f_name.get(),
+                      'l_name': l_name.get(),
+                      'address': address.get(),
+                      'city': city.get(),
+                      'state': state.get(),
+                      'zipcode': zipcode.get()
+                  }
+                  )
+    
     f_name.delete(0, END)
     l_name.delete(0, END)
     address.delete(0, END)
     city.delete(0, END)
     state.delete(0, END)
     zipcode.delete(0, END)
+    
+    conn.commit()
+    conn.close()
+    
+def query():
+    conn = sqlite3.connect('address_book.db')
+    cursr = conn.cursor()
+    cursr.execute("SELECT *, oid FROM addresses")
+    records = cursr.fetchall()
+    # cursr.fetchone()
+    # cursr.fetchmany(50)
+    print(records)
+    print_records = ""
+    for record in records:
+        print_records += str(record) + "\n"
+    
+    query_label = Label(root, text=print_records)
+    query_label.grid(row=8, column=0, columnspan=2)
+    conn.commit()
+    conn.close()
 
 submt_btn = Button(root, text="Add Record to Database", command=submit)
 submt_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
+query_btn = Button(root, text="Show Records", command=query)
+query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
 
-conn.commit()
-conn.close()
+def delete(name):
+    conn = sqlite3.connect('address_book.db')
+    cursr = conn.cursor()
+    cursr.execute(f"DELETE FROM addresses WHERE first_name='{name}'")
+    conn.commit()
+    conn.close()
+    
+entry = Entry(root, width=50)
+entry.grid(row=8, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
+    
+delete_btn = Button(root, text="Delete", command=delete(entry.get()))
+delete_btn.grid(row=9, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
 
 root.mainloop()
